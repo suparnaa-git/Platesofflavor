10e5888c1fbd013474b45a20da9ebb4155543318
suparnaa-git <suparna2000chowdhury@gmail.com>
2021-08-12 15:42:17 +0530

    make it better

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,15 @@ import { SingUpComponent } from './Components/Auth/sing-up/sing-up.component';
 import { HomeComponent } from './Components/home/home.component';
 import { PageNotFoundComponent } from './Layout/page-not-found/page-not-found.component';
 import { ContactusComponent } from './Components/contactus/contactus.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
 {path:'',redirectTo:'/about',pathMatch:'full'},
 {path:'home',component:HomeComponent},
-{path:'about', component:AboutComponent},
+{path:'about', component:AboutComponent,canActivate:[AuthGuard]},
 {path:'signin',component:SingInComponent},
 {path:'signup',component:SingUpComponent},
-{path:'contactus',component:ContactusComponent},
+{path:'contactus',component:ContactusComponent,canActivate:[AuthGuard]},
 { path: '**', component: PageNotFoundComponent }];
 
 @NgModule({
